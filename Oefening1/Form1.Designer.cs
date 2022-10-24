99d6a653144df4905f2b67d4bcf174c9a5361105
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-10-24 15:46:52 +0200

    Form Design

--- Oefening1/Form1.Designer.cs
+++ Oefening1/Form1.Designer.cs
@@ -28,76 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.inputTitleTB = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.inputTitle = new System.Windows.Forms.Label();
+            this.informationInput = new System.Windows.Forms.Label();
+            this.msgBox = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.titleTB = new System.Windows.Forms.TextBox();
+            this.informationTB = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.timeCB = new System.Windows.Forms.CheckBox();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.nextTaskBtn = new System.Windows.Forms.Button();
+            this.afgewerktBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // inputTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(181, 95);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Te Doen";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.inputTitle.AutoSize = true;
+            this.inputTitle.Location = new System.Drawing.Point(93, 81);
+            this.inputTitle.Name = "inputTitle";
+            this.inputTitle.Size = new System.Drawing.Size(74, 25);
+            this.inputTitle.TabIndex = 0;
+            this.inputTitle.Text = "Te doen";
             // 
-            // inputTitleTB
+            // informationInput
             // 
-            this.inputTitleTB.Location = new System.Drawing.Point(45, 142);
-            this.inputTitleTB.Name = "inputTitleTB";
-            this.inputTitleTB.Size = new System.Drawing.Size(416, 31);
-            this.inputTitleTB.TabIndex = 1;
+            this.informationInput.AutoSize = true;
+            this.informationInput.Location = new System.Drawing.Point(498, 81);
+            this.informationInput.Name = "informationInput";
+            this.informationInput.Size = new System.Drawing.Size(125, 25);
+            this.informationInput.TabIndex = 1;
+            this.informationInput.Text = "Mededelingen";
             // 
-            // textBox1
+            // msgBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(45, 238);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(416, 198);
-            this.textBox1.TabIndex = 2;
+            this.msgBox.AutoSize = true;
+            this.msgBox.Location = new System.Drawing.Point(93, 426);
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(125, 25);
+            this.msgBox.TabIndex = 2;
+            this.msgBox.Text = "Message Box: ";
             // 
-            // dateTimePicker1
+            // infoLabel
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(56, 469);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(359, 31);
-            this.dateTimePicker1.TabIndex = 3;
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(498, 426);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(115, 25);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "Information: ";
+            // 
+            // titleTB
+            // 
+            this.titleTB.Location = new System.Drawing.Point(93, 125);
+            this.titleTB.Multiline = true;
+            this.titleTB.Name = "titleTB";
+            this.titleTB.Size = new System.Drawing.Size(332, 184);
+            this.titleTB.TabIndex = 4;
+            // 
+            // informationTB
+            // 
+            this.informationTB.Location = new System.Drawing.Point(498, 125);
+            this.informationTB.Multiline = true;
+            this.informationTB.Name = "informationTB";
+            this.informationTB.Size = new System.Drawing.Size(332, 184);
+            this.informationTB.TabIndex = 5;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(93, 315);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(300, 31);
+            this.dateTimePicker.TabIndex = 6;
+            // 
+            // timeCB
+            // 
+            this.timeCB.AutoSize = true;
+            this.timeCB.Location = new System.Drawing.Point(399, 315);
+            this.timeCB.Name = "timeCB";
+            this.timeCB.Size = new System.Drawing.Size(76, 29);
+            this.timeCB.TabIndex = 7;
+            this.timeCB.Text = "Time";
+            this.timeCB.UseVisualStyleBackColor = true;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(93, 370);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(112, 34);
+            this.SaveBtn.TabIndex = 8;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            // 
+            // nextTaskBtn
+            // 
+            this.nextTaskBtn.Location = new System.Drawing.Point(313, 370);
+            this.nextTaskBtn.Name = "nextTaskBtn";
+            this.nextTaskBtn.Size = new System.Drawing.Size(112, 34);
+            this.nextTaskBtn.TabIndex = 9;
+            this.nextTaskBtn.Text = "Next";
+            this.nextTaskBtn.UseVisualStyleBackColor = true;
+            // 
+            // afgewerktBtn
+            // 
+            this.afgewerktBtn.Location = new System.Drawing.Point(532, 370);
+            this.afgewerktBtn.Name = "afgewerktBtn";
+            this.afgewerktBtn.Size = new System.Drawing.Size(112, 34);
+            this.afgewerktBtn.TabIndex = 10;
+            this.afgewerktBtn.Text = "Afgewerkt";
+            this.afgewerktBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(708, 370);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(112, 34);
+            this.addBtn.TabIndex = 11;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(239, 421);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(112, 34);
+            this.btn1.TabIndex = 12;
+            this.btn1.Text = "Off";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(657, 417);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(112, 34);
+            this.btn2.TabIndex = 13;
+            this.btn2.Text = "Off";
+            this.btn2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1368, 770);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.inputTitleTB);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(832, 515);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.afgewerktBtn);
+            this.Controls.Add(this.nextTaskBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.timeCB);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.informationTB);
+            this.Controls.Add(this.titleTB);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.msgBox);
+            this.Controls.Add(this.informationInput);
+            this.Controls.Add(this.inputTitle);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-
-        #endregion
-
         private Label inputTitle;
         private Label informationInput;
+        private Label msgBox;
+        private Label infoLabel;
         private TextBox titleTB;
         private TextBox informationTB;
         private DateTimePicker dateTimePicker;
         private CheckBox timeCB;
         private Button SaveBtn;
         private Button nextTaskBtn;
-        private Button addBtn;
         private Button afgewerktBtn;
+        private Button addBtn;
         private Button btn1;
-        private Label msgBox;
-        private Label infoLabel;
         private Button btn2;
+
+
+        #endregion
+
+        //private Label inputTitle;
+        //private Label informationInput;
+        //private TextBox titleTB;
+        //private TextBox informationTB;
+        //private DateTimePicker dateTimePicker;
+        //private CheckBox timeCB;
+        //private Button SaveBtn;
+        //private Button nextTaskBtn;
+        //private Button addBtn;
+        //private Button afgewerktBtn;
+        //private Button btn1;
+        //private Label msgBox;
+        //private Label infoLabel;
+        //private Button btn2;
     }
 }
